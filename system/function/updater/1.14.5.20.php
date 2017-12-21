e68b8ff5328792185a078680e096a6d9c73c4454
fewewf <32592150+fewewf@users.noreply.github.com>
2017-12-21 11:24:05 +0800

    f

--- system/function/updater/1.14.5.20.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(!defined('IN_KKFRAME')) exit('Access Denied');
-DB::query('DROP TABLE update_source');
-saveSetting('version', '1.14.5.27');
-showmessage('成功更新到 1.14.5.27！', './');
-?>
--- system/function/updater/1.14.6.2.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!defined('IN_KKFRAME')) exit('Access Denied');
-
-DB::query("ALTER TABLE `member` MODIFY `password` VARCHAR(255)");
-DB::query("UPDATE `setting` SET `k`='jquery_mode', `v`='builtin' WHERE (`k`='jquery_mode')");
-
-saveSetting('version', '1.14.6.3');
-showmessage('成功更新到 1.14.6.3！', './');
