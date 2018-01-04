65e58c949be5b05d223816399e2bf3dff4af69b4
fewewf <32592150+fewewf@users.noreply.github.com>
2018-01-05 05:47:16 +0800

    1.6

---
+++ system/config.inc.php
@@ -0,0 +1,15 @@
+<?php
+/* Auto-generated config file */
+$_config = array (
+  'version' => '1.14.6.4',
+  'db' => 
+  array (
+    'server' => '172.30.216.186',
+    'port' => 3306,
+    'username' => 'data',
+    'password' => 'data',
+    'name' => 'data',
+    'pconnect' => false,
+  ),
+);
+?>
